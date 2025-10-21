9d935f76720959fd1ee80afffa35c26b83fd7b77
Junichiro Sugihara <j-sugihara@jsk.imi.i.u-tokyo.ac.jp>
2025-10-21 14:49:15 +0900

    [Dual duct][WIP] Add new case of dual ducted fan

---
+++ dual_ducted_fan/constant/geometry/temp.sh
@@ -0,0 +1,14 @@
+surfaceTransformPoints "translate=(0.19 0 -0.2)" propeller.obj                   propeller_A.obj &
+surfaceTransformPoints "translate=(0.19 0 -0.2)" duct.obj                        duct_A.obj &
+surfaceTransformPoints "translate=(0.19 0 -0.2)" propeller-innerCylinder.obj     propeller-innerCylinder_A.obj &
+surfaceTransformPoints "translate=(0.19 0 -0.2)" propeller-middleCylinder.obj propeller-middleCylinder_A.obj &
+# ==== B ( -0.19 0 -0.2 ) ====
+surfaceTransformPoints "translate=(-0.19 0 -0.2)" propeller.obj                   propeller_B.obj &
+surfaceTransformPoints "translate=(-0.19 0 -0.2)" duct.obj                        duct_B.obj &
+surfaceTransformPoints "translate=(-0.19 0 -0.2)" propeller-innerCylinder.obj     propeller-innerCylinder_B.obj &
+surfaceTransformPoints "translate=(-0.19 0 -0.2)" propeller-middleCylinder.obj propeller-middleCylinder_B.obj &
+# ==== C ( 0 0 0.2 ) ==== 
+surfaceTransformPoints "translate=(0 0 0.2)" propeller.obj                        propeller_C.obj &
+surfaceTransformPoints "translate=(0 0 0.2)" duct.obj                             duct_C.obj &
+surfaceTransformPoints "translate=(0 0 0.2)" propeller-innerCylinder.obj          propeller-innerCylinder_C.obj &
+surfaceTransformPoints "translate=(0 0 0.2)" propeller-middleCylinder.obj      propeller-middleCylinder_C.obj &
